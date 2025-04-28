fee15df5de5899287592303de8f1b25ee515f834
Guilherme Brogio Macedo da Silva <79169549+gbrogio@users.noreply.github.com>
2025-04-28 11:15:30 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [{ hostname: 'txbzeyhyrceumtbddgye.supabase.co' }],
+    loader: 'default', // Prevents Vercel's optimization
+    unoptimized: true, // Disables all image optimizations globally
   },
 };
 
